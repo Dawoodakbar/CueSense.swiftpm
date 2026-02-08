6ac9df64de402c1e7af51db45d446daca9caf573
Dawoodakbar <uzumakiminato806@gmail.com>
2026-02-08 20:52:02 +0500

    made the onboardingviews, with the user's interest to show him that type of cues

--- MyApp.swift
+++ MyApp.swift
@@ -5,12 +5,16 @@ import SwiftUI
 @main
 struct MyApp: App {
     @StateObject private var speechManager = SpeechManager()
+    @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(speechManager)
+                .fullScreenCover(isPresented: .constant(!hasCompletedOnboarding)) {
+                    OnboardingContainerView()
+                }
         }
-        .modelContainer(for: InteractionSession.self)
+        .modelContainer(for: [InteractionSession.self, UserProfile.self])
     }
 }
